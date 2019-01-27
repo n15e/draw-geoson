3478b5ae681d2193847fa5a7c21a6a10442d9f40
David Gilbertson <gilbertson.david@gmail.com>
2019-01-28 10:04:24 +1100

    Extract reusable snap logic

--- src/Map/SnapMode.js
+++ src/Map/SnapMode.js
@@ -1,164 +1,9 @@
 import Constants from '@mapbox/mapbox-gl-draw/src/constants';
 import doubleClickZoom from '@mapbox/mapbox-gl-draw/src/lib/double_click_zoom';
 import DrawPolygon from '@mapbox/mapbox-gl-draw/src/modes/draw_polygon';
+import { addPointToGuides, findGuidesFromFeatures, IDS, roundLngLatTo1Cm, snap } from './snapUtils';
 
 const SnapMode = {...DrawPolygon};
-
-const IDS = {
-  VERTICAL_LINE_GUIDE: 'VERTICAL_LINE_GUIDE',
-  HORIZONTAL_LINE_GUIDE: 'HORIZONTAL_LINE_GUIDE',
-};
-
-/**
- * Takes a point and maybe adds it to the list of guides.
- *
- * Mutates guides.
- *
- * @param guides
- * @param point
- */
-const addPointToGuides = (guides, point) => {
-  // Round to the nearest pixel
-  const x = Math.round(point.x);
-  const y = Math.round(point.y);
-
-  // Don't add points not visible on the page (it's annoying)
-  if (x > 0 && x < window.innerWidth && y > 0 && y < window.innerHeight) {
-    // Don't add duplicates
-    if (!guides.vertical.includes(x)) guides.vertical.push(x);
-    if (!guides.horizontal.includes(y)) guides.horizontal.push(y);
-  }
-};
-
-/**
- * Loops over all features to get vertical and horizontal guides to snap to
- *
- * @param map
- * @param draw
- * @param polygonId
- * @returns {{vertical: Array, horizontal: Array}}
- */
-const findGuidesFromFeatures = (map, draw, polygonId) => {
-  const features = draw.getAll().features;
-
-  // TODO (davidg): re-run this on map pan or zoom (or screen resize?)
-  const guides = {
-    vertical: [],
-    horizontal: [],
-  };
-
-  features.forEach(feature => {
-    // If this is re-running because a user is moving the map, the features might include
-    // guides or the last leg of a polygon
-    if (feature.id === IDS.HORIZONTAL_LINE_GUIDE || feature.id === IDS.VERTICAL_LINE_GUIDE) return;
-
-    const getCoordinates = array => {
-      if (!Array.isArray(array)) throw Error('Your array is not an array');
-
-      if (Array.isArray(array[0])) {
-        // This is an array of arrays, we must go deeper
-        array.forEach(getCoordinates);
-      } else {
-        // If not an array of array, only consider arrays with two items
-        if (array.length === 2) {
-          const point = map.project(array);
-
-          addPointToGuides(guides, point);
-        }
-      }
-    };
-
-    // For the current polygon, the last two points are the mouse position and back home
-    // so we chop those off (else we get guides showing where the user clicked, even
-    // if they were just panning the map)
-    const coordinates = feature.id === polygonId
-      ? feature.geometry.coordinates[0].slice(0, -2)
-      : feature.geometry.coordinates;
-
-    getCoordinates(coordinates);
-  });
-
-  return guides;
-};
-
-/**
- * For a given point, this returns any vertical and/or horizontal guides within snapping distance
- *
- * @param guides
- * @param point
- * @param snapPx
- * @returns {{vertical: number | undefined, horizontal: number | undefined}}
- */
-const getNearbyGuides = (guides, point, snapPx) => {
-  const nearbyVerticalGuide = guides.vertical.find(guide => (
-    Math.abs(guide - point.x) < snapPx
-  ));
-
-  const nearbyHorizontalGuide = guides.horizontal.find(guide => (
-    Math.abs(guide - point.y) < snapPx
-  ));
-
-  return {
-    verticalPx: nearbyVerticalGuide,
-    horizontalPx: nearbyHorizontalGuide,
-  }
-};
-
-/**
- * Returns snap points if there are any, otherwise the original lng/lat of the event
- * Also, defines if guides should show on the state object
- *
- * Mutates the state object
- *
- * @param state
- * @param e
- * @returns {{lng: number, lat: number}}
- */
-const snap = (state, e) => {
-  let lng = e.lngLat.lng;
-  let lat = e.lngLat.lat;
-
-  const { verticalPx, horizontalPx } = getNearbyGuides(state.guides, e.point, state.snapPx);
-
-  if (verticalPx) {
-    // Draw a line from top to bottom
-    const lngLatTop = state.map.unproject({x: verticalPx, y: 0});
-    const lngLatBottom = state.map.unproject({x: verticalPx, y: window.innerHeight});
-    const lngLatPoint = state.map.unproject({x: verticalPx, y: e.point.y});
-
-    state.verticalLine.updateCoordinate(0, lngLatTop.lng, lngLatTop.lat);
-    state.verticalLine.updateCoordinate(1, lngLatBottom.lng, lngLatBottom.lat);
-
-    lng = lngLatPoint.lng;
-    lat = lngLatPoint.lat;
-  }
-
-  if (horizontalPx) {
-    // Draw a line from left to right
-    const lngLatLeft = state.map.unproject({x: 0, y: horizontalPx});
-    const lngLatRight = state.map.unproject({x: window.innerWidth, y: horizontalPx});
-    const lngLatPoint = state.map.unproject({x: e.point.x, y: horizontalPx});
-
-    state.horizontalLine.updateCoordinate(0, lngLatLeft.lng, lngLatLeft.lat);
-    state.horizontalLine.updateCoordinate(1, lngLatRight.lng, lngLatRight.lat);
-
-    lng = lngLatPoint.lng;
-    lat = lngLatPoint.lat;
-  }
-
-  if (verticalPx && horizontalPx) {
-    // For rather complicated reasons, we need to explicitly set both so it behaves on a rotated map
-    const lngLatPoint = state.map.unproject({x: verticalPx, y: horizontalPx});
-
-    lng = lngLatPoint.lng;
-    lat = lngLatPoint.lat;
-  }
-
-  state.showVerticalSnapLine = !!verticalPx;
-  state.showHorizontalSnapLine = !!horizontalPx;
-
-  return { lng, lat };
-};
 
 SnapMode.onSetup = function({ snapPx = 10, draw }) {
   const polygon = this.newFeature({
@@ -170,12 +15,12 @@ SnapMode.onSetup = function({ snapPx = 10, draw }) {
     },
   });
 
-  // Our temporary guide lines
+  // Temporary guide lines
   const verticalLine = this.newFeature({
     id: IDS.VERTICAL_LINE_GUIDE,
     type: Constants.geojsonTypes.FEATURE,
     properties: {
-      isSnapGuide: 'true',
+      isSnapGuide: 'true', // for styling
     },
     geometry: {
       type: Constants.geojsonTypes.LINE_STRING,
@@ -214,7 +59,7 @@ SnapMode.onSetup = function({ snapPx = 10, draw }) {
   };
 
   this.map.on('moveend', () => {
-    // Update the guides after zoom, pan, rotate, and resize
+    // Update the guide locations after zoom, pan, rotate, or resize
     state.guides = findGuidesFromFeatures(this.map, draw, polygon.id);
   });
 
@@ -222,22 +67,28 @@ SnapMode.onSetup = function({ snapPx = 10, draw }) {
 };
 
 SnapMode.onClick = function(state) {
-  // End the thing when the click is on the previous position
+  // We save some processing by rounding on click, not mousemove
+  const lng = roundLngLatTo1Cm(state.snappedLng);
+  const lat = roundLngLatTo1Cm(state.snappedLat);
+
+  // End the drawing if this click is on the previous position
   if (state.currentVertexPosition > 0) {
     const lastVertex = state.polygon.coordinates[0][state.currentVertexPosition - 1];
 
-    if (lastVertex[0] === state.snappedLng && lastVertex[1] === state.snappedLat) {
-      this._deleteGuides(state);
+    if (lastVertex[0] === lng && lastVertex[1] === lat) {
       return this.changeMode(Constants.modes.SIMPLE_SELECT, { featureIds: [state.polygon.id] });
     }
   }
 
-  const point = state.map.project({lng: state.snappedLng, lat: state.snappedLat});
+  const point = state.map.project({lng: lng, lat: lat});
+
   addPointToGuides(state.guides, point);
 
-  state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, state.snappedLng, state.snappedLat);
+  state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, lng, lat);
+
   state.currentVertexPosition++;
-  state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, state.snappedLng, state.snappedLat);
+
+  state.polygon.updateCoordinate(`0.${state.currentVertexPosition}`, lng, lat);
 };
 
 SnapMode.onMouseMove = function(state, e) {
@@ -248,11 +99,6 @@ SnapMode.onMouseMove = function(state, e) {
   state.snappedLat = lat;
 
   this.updateUIClasses({ mouse: Constants.cursors.ADD });
-};
-
-SnapMode._deleteGuides = function() {
-  this.deleteFeature(IDS.VERTICAL_LINE_GUIDE, { silent: true });
-  this.deleteFeature(IDS.HORIZONTAL_LINE_GUIDE, { silent: true });
 };
 
 // This is 'extending' DrawPolygon.toDisplayFeatures
@@ -271,7 +117,8 @@ SnapMode.toDisplayFeatures = function(state, geojson, display) {
 
 // This is 'extending' DrawPolygon.onStop
 SnapMode.onStop = function(state) {
-  this._deleteGuides(state);
+  this.deleteFeature(IDS.VERTICAL_LINE_GUIDE, { silent: true });
+  this.deleteFeature(IDS.HORIZONTAL_LINE_GUIDE, { silent: true });
 
   // This relies on the the state of SnapMode being similar to DrawPolygon
   DrawPolygon.onStop.call(this, state);
---
+++ src/Map/snapUtils.js
@@ -0,0 +1,186 @@
+// Number of decimal places in lat/lng values
+// One day could be configurable
+export const ACCURACY = {
+  '1 m': 5,
+  '10 cm': 6,
+  '1 cm': 7,
+  '1 mm': 8,
+};
+
+export const IDS = {
+  VERTICAL_LINE_GUIDE: 'VERTICAL_LINE_GUIDE',
+  HORIZONTAL_LINE_GUIDE: 'HORIZONTAL_LINE_GUIDE',
+};
+
+const round = (num, decimals) => Math.round(num * 10 ** decimals) / 10 ** decimals;
+
+// There's not much point in 13 decimal places of lng/lat,
+// so we round to the nearest cm when storing the lat/lng
+export const roundLngLatTo1Cm = num => round(num, ACCURACY['1 cm']);
+
+/**
+ * Takes a point and maybe adds it to the list of guides.
+ *
+ * Mutates guides.
+ *
+ * @param {object} guides
+ * @param {Array<number>} guides.vertical
+ * @param {Array<number>} guides.horizontal
+ * @param {object} point
+ * @param {number} point.x
+ * @param {number} point.y
+ * @param {boolean} [forceInclusion]
+ */
+export const addPointToGuides = (guides, point, forceInclusion) => {
+  // Round to the nearest pixel, reduces the number of guides and also is just sensible
+  const x = Math.round(point.x);
+  const y = Math.round(point.y);
+
+  // Don't add points not visible on the page (it's annoying)
+  const pointIsOnTheScreen = x > 0 && x < window.innerWidth && y > 0 && y < window.innerHeight;
+
+  // But do add off-screen points if forced (e.g. for the current feature)
+  // So features will always snap to their own points
+  if (pointIsOnTheScreen || forceInclusion) {
+    // Don't add duplicates
+    if (!guides.vertical.includes(x)) guides.vertical.push(x);
+    if (!guides.horizontal.includes(y)) guides.horizontal.push(y);
+  }
+};
+
+/**
+ * Loops over all features to get vertical and horizontal guides to snap to
+ *
+ * @param map
+ * @param draw
+ * @param polygonId
+ * @returns {{vertical: Array, horizontal: Array}}
+ */
+export const findGuidesFromFeatures = (map, draw, polygonId) => {
+  const features = draw.getAll().features;
+
+  const guides = {
+    vertical: [],
+    horizontal: [],
+  };
+
+  features.forEach(feature => {
+    const isTheCurrentPolygon = feature.id === polygonId;
+
+    // If this is re-running because a user is moving the map, the features might include
+    // guides or the last leg of a polygon
+    if (feature.id === IDS.HORIZONTAL_LINE_GUIDE || feature.id === IDS.VERTICAL_LINE_GUIDE) return;
+
+    const getCoordinates = array => {
+      if (!Array.isArray(array)) throw Error('Your array is not an array');
+
+      if (Array.isArray(array[0])) {
+        // This is an array of arrays, we must go deeper
+        array.forEach(getCoordinates);
+      } else {
+        // If not an array of arrays, only consider arrays with two items
+        if (array.length === 2) {
+          addPointToGuides(guides, map.project(array), isTheCurrentPolygon);
+        }
+      }
+    };
+
+    // For the current polygon, the last two points are the mouse position and back home
+    // so we chop those off (else we get guides showing where the user clicked, even
+    // if they were just panning the map)
+    const coordinates = isTheCurrentPolygon
+      ? feature.geometry.coordinates[0].slice(0, -2)
+      : feature.geometry.coordinates;
+
+    getCoordinates(coordinates);
+  });
+
+  return guides;
+};
+
+/**
+ * For a given point, this returns any vertical and/or horizontal guides within snapping distance
+ *
+ * @param guides
+ * @param point
+ * @param snapPx
+ * @returns {{vertical: number | undefined, horizontal: number | undefined}}
+ */
+const getNearbyGuides = (guides, point, snapPx) => {
+  const nearbyVerticalGuide = guides.vertical.find(guide => (
+    Math.abs(guide - point.x) < snapPx
+  ));
+
+  const nearbyHorizontalGuide = guides.horizontal.find(guide => (
+    Math.abs(guide - point.y) < snapPx
+  ));
+
+  return {
+    verticalPx: nearbyVerticalGuide,
+    horizontalPx: nearbyHorizontalGuide,
+  }
+};
+
+/**
+ * Returns snap points if there are any, otherwise the original lng/lat of the event
+ * Also, defines if guides should show on the state object
+ *
+ * Mutates the state object
+ *
+ * @param state
+ * @param e
+ * @returns {{lng: number, lat: number}}
+ */
+export const snap = (state, e) => {
+  let lng = e.lngLat.lng;
+  let lat = e.lngLat.lat;
+
+  // Holding alt bypasses all snapping
+  if (e.originalEvent.altKey) {
+    state.showVerticalSnapLine = false;
+    state.showHorizontalSnapLine = false;
+
+    return { lng, lat };
+  }
+
+  const { verticalPx, horizontalPx } = getNearbyGuides(state.guides, e.point, state.snapPx);
+
+  if (verticalPx) {
+    // Draw a line from top to bottom
+    const lngLatTop = state.map.unproject({x: verticalPx, y: 0});
+    const lngLatBottom = state.map.unproject({x: verticalPx, y: window.innerHeight});
+    const lngLatPoint = state.map.unproject({x: verticalPx, y: e.point.y});
+
+    state.verticalLine.updateCoordinate(0, lngLatTop.lng, lngLatTop.lat);
+    state.verticalLine.updateCoordinate(1, lngLatBottom.lng, lngLatBottom.lat);
+
+    lng = lngLatPoint.lng;
+    lat = lngLatPoint.lat;
+  }
+
+  if (horizontalPx) {
+    // Draw a line from left to right
+    const lngLatLeft = state.map.unproject({x: 0, y: horizontalPx});
+    const lngLatRight = state.map.unproject({x: window.innerWidth, y: horizontalPx});
+    const lngLatPoint = state.map.unproject({x: e.point.x, y: horizontalPx});
+
+    state.horizontalLine.updateCoordinate(0, lngLatLeft.lng, lngLatLeft.lat);
+    state.horizontalLine.updateCoordinate(1, lngLatRight.lng, lngLatRight.lat);
+
+    lng = lngLatPoint.lng;
+    lat = lngLatPoint.lat;
+  }
+
+  if (verticalPx && horizontalPx) {
+    // For rather complicated reasons, we need to explicitly set both so it behaves on a rotated map
+    const lngLatPoint = state.map.unproject({x: verticalPx, y: horizontalPx});
+
+    lng = lngLatPoint.lng;
+    lat = lngLatPoint.lat;
+  }
+
+  state.showVerticalSnapLine = !!verticalPx;
+  state.showHorizontalSnapLine = !!horizontalPx;
+
+  return { lng, lat };
+};
